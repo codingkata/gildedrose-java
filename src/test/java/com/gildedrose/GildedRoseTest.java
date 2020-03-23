8b2a44439a28808f6c76eab2ebbb6004f3e041df
qiaoliang <sagittatius.qiao@gmail.com>
2020-03-23 13:56:43 +0800

    approval test.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,24 @@
 package com.gildedrose;
 
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void foo() throws Exception {
+        CombinationApprovals.verifyAllCombinations(
+                this::getUpdateResult,
+                new String[]{"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"},
+                new Integer[]{-1,0,11},
+                new Integer[]{0,1,49,50});
+    }
+
+    String getUpdateResult(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        return app.items[0].toString();
     }
 }
