7448f96a70c3a1ddb780801ac888fce21ebc6f6d
qiaoliang <sagittatius.qiao@gmail.com>
2020-03-30 16:27:43 +0800

    使用AprovalTest 框架，为方便下一步增加更多的测试用例做准备。

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,8 @@
 package com.gildedrose;
 
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,10 +11,17 @@ public class GildedRoseTest {
 
     @Test
     public void update_test() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        String[] names=new String[]{"foo"};
+        Integer[] sellIns = new Integer[]{0};
+        Integer[] qualities= new Integer[]{0};
+        CombinationApprovals.verifyAllCombinations(this::getUpdateItemResult,names,sellIns,qualities);
+    }
+
+    private String getUpdateItemResult(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo, -1, 0", app.items[0].toString());
+        return app.items[0].toString();
     }
 
 }
