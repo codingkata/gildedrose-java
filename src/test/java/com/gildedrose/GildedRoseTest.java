efcc1895a80d8b11c493116cf3ea58e776056fea
qiaoliang <sagittatius.qiao@gmail.com>
2020-03-30 16:11:59 +0800

    测试中使用toString(), 验证更有意义的内容，同时出增加了测试覆盖率。

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,11 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void update_test() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals("foo, -1, 0", app.items[0].toString());
     }
 
 }
