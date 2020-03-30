443a2a6fa2641047bb5f4bdcc360f155426490fc
qiaoliang <sagittatius.qiao@gmail.com>
2020-03-30 19:41:08 +0800

    hit 100% coverage.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,9 +11,15 @@ public class GildedRoseTest {
 
     @Test
     public void update_test() {
-        String[] names= new String[]{"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"};
-        Integer[] sellIns = new Integer[]{0};
-        Integer[] qualities= new Integer[]{0};
+        String[] names=
+                new String[]{
+                        "foo",
+                        "Aged Brie",
+                        "Backstage passes to a TAFKAL80ETC concert",
+                        "Sulfuras, Hand of Ragnaros"
+        };
+        Integer[] sellIns = new Integer[]{-1,0,11};
+        Integer[] qualities= new Integer[]{0,1,49,50};
         CombinationApprovals.verifyAllCombinations(this::getUpdateItemResult,names,sellIns,qualities);
     }
     private String getUpdateItemResult(String name, int sellIn, int quality) {
